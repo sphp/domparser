73a07607c282f1d8e6010b06a86ec7b3eff340fc
Shamim <35076893+sphp@users.noreply.github.com>
2019-12-30 23:14:54 +0600

    Create dom.php

---
+++ dom.php
@@ -0,0 +1,244 @@
+<?php
+class Dom{
+	public $doc;
+	static $parsedUrl;
+	static $data;
+	static $temp='./temp/';
+	function __construct(){
+		if($this->doc === null){
+			$this->doc = new DOMDocument();
+			libxml_use_internal_errors(true);
+		}
+		if(self::$data) $this->doc->loadHTML(self::$data);
+	}
+	static function __callStatic($func, $args){
+		$data = $args[0];
+		if(filter_var($data, FILTER_VALIDATE_URL)){
+			self::$parsedUrl = parse_url($data);
+			self::getData($data);
+		}
+		else self::$func = $data;
+		return new self;
+	}
+	static function getData($url, $expire=86400/*24hr*/){
+		extract(self::$parsedUrl);
+		$path = self::absPath(self::$temp . $host) . DIRECTORY_SEPARATOR;
+		//create directory if not exists
+		if (!file_exists($path)) mkdir($path, 0777, true);
+		//create data file name from target url
+		$path .= !empty(basename($url)) ? basename($url) : $host.'.html';
+		if(is_readable($path) && time()-filemtime($path) < $expire){
+			self::$data = file_get_contents($path);
+		}else{
+			$output = self::curlGet($url, 1);
+			if($output['info']['http_code']==200){
+				self::$data = $output['data'];
+				file_put_contents($path, $output['data']);
+			}
+		}
+		return !empty(self::$data) ? self::$data : false;
+	}
+	static function absPath($path){
+		$path = str_replace(array('/', '\\'), 'DS', $path);
+		$relDirs = array_filter(explode('DS', $path), function($p){return $p!=='.';});
+		$absDirs = explode(DIRECTORY_SEPARATOR, getcwd());
+		foreach($relDirs as $dir) $dir=='..' ? array_pop($absDirs) : array_push($absDirs, $dir);
+		return implode(DIRECTORY_SEPARATOR, $absDirs);
+	}
+	static function curlGet($url, $info=0){
+		$result = [];
+		$ch = curl_init($url);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+		curl_setopt($ch, CURLOPT_ENCODING, "");
+		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
+		curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+		curl_setopt($ch, CURLOPT_FAILONERROR, 1);
+		$result = curl_exec($ch);
+		$cinfo  = curl_getinfo($ch);
+		curl_close($ch);
+		if($cinfo['http_code']!=200) return $cinfo;
+		return $info ? ['info' => $cinfo, 'data' => $result] : $result;
+	}
+	static function inrHTML(DOMNode $element=null){
+		$innerHTML="";
+		$children =$element->childNodes;
+		foreach($children as $child)
+		$innerHTML .= $element->ownerDocument->saveHTML($child);
+		return trim($innerHTML);
+	}
+	static function make_clickable($text){
+		$regex = '#\bhttps?://[^\s()<>]+(?:\([\w\d]+\)|([^[:punct:]\s]|/))#';
+		return preg_replace_callback($regex, function ($matches) {
+			return "<a href=\'{$matches[0]}\'>{$matches[0]}</a>";
+		},$text);
+	}
+	static function in2strs($data, $start, $end, $callback=null){
+		$p=array();
+		$p1=explode($start,$data);
+		for($i=1,$c=count($p1) ; $i<$c ; $i++){
+			$p2 = explode($end,$p1[$i])[0];
+			if($callback) call_user_func($callback, $p2);
+			$p[] = $p2;
+		}
+		return $p;
+	}
+	static function strBetween($str, $from, $to, $callback=null){
+		$string = substr($str, strpos($str, $from) + strlen($from));
+		if(strstr($string, $to,true)!=false) $string = strstr ($string,$to,true);
+		return $string;
+	}
+	function __call($func, $args){
+		if(method_exists($this->doc, $func))
+			return call_user_func_array(array($this->doc, $func), $args);
+		else{
+			if(strpos($func, '_')!==false) $func = str_replace("_", '-', $func);
+			str_replace("'", '"', self::$data);
+			return array_filter(self::in2strs(self::$data, $func."='" ,"'"));
+		}
+	}
+	static function getUrls($data){
+		return matches('#\bhttps?://[^,\s()<>]+(?:\([\w\d]+\)|([^,[:punct:]\s]|/))#', $data)[0];
+	}
+	function urls(){
+		return array_map(function($str){return str_replace(['"','\''],'',explode('>',$str,2)[0]);}, self::getUrls(self::$data));
+	}
+	function find($selectr){
+		$xp = $this->xPath($selectr);
+		$xh = new DOMXpath($this->doc);
+		$this->nodeList = $xh->query($xp);
+		return $this;
+	}
+	function loadHTML($html){
+		$this->doc->loadHTML( $html );
+	}
+	function loadURL($url){
+		$this->content = $this->get_contents($url);
+		$this->doc->loadHTML( $this->content );
+	}
+	function nodes(){
+		return $this->nodeList;
+	}
+	function node($indx=0){
+		return $this->nodeList->item($indx);
+	}
+	function attr($val){
+		$arr=[];
+		foreach($this->nodeList as $node) $arr[]=$node->getAttribute($val);
+		return array_filter($arr);
+	}
+	function text(){
+		$arr=[];
+		foreach($this->nodeList as $node) $arr[]=trim($node->textContent);
+		return $arr;
+	}
+	function GetById($id){
+		$this->nodeList=$this->doc->getElementById($id);
+		return $this;
+	}
+	function GetByTagName($tag){
+		$this->nodeList = $this->doc->getElementsByTagName($tag);
+		return $this;
+	}
+	function GetByClass($class){
+		$xpath=new DOMXPath($this->doc);
+		$this->nodeList=$xpath->query( "//*[contains(concat(' ',	normalize-space(@class),' '),' $class ')]" );
+		return $this;
+	}
+	function innerHTML(){
+		$arr=[];
+		foreach($this->nodeList as $node) $arr[]=trim(self::inrHTML($node));
+		return array_filter($arr);
+	}
+	function save($fpath){
+		return $this->doc->saveHTMLFile($fpath);
+	}
+	function xPath($sl){
+		//remove spaces around operators
+		$sl=preg_replace('/\s*>\s*/','>',$sl);
+		$sl=preg_replace('/\s*~\s*/','~',$sl);
+		$sl=preg_replace('/\s*\+\s*/','+',$sl);
+		$sl=preg_replace('/\s*,\s*/',',',$sl);
+		$sls=preg_split('/\s+(?![^\[]+\])/',$sl);
+		foreach($sls as &$sl){
+			$sl=preg_replace('/,/','|descendant-or-self::',$sl);// ,
+			$sl=preg_replace('/(.+)?:(checked|disabled|required|autofocus)/','\1[@\2="\2"]',$sl);//input:checked, :disabled, etc.
+			$sl=preg_replace('/(.+)?:(autocomplete)/','\1[@\2="on"]',$sl);//input:autocomplete, :autocomplete
+			$sl=preg_replace('/:(text|password|checkbox|radio|button|submit|reset|file|hidden|image|datetime|datetime-local|date|month|time|week|number|range|email|url|search|tel|color)/','input[@type="\1"]',$sl);//input:button, input:submit, etc.
+			$sl=preg_replace('/(\w+)\[([_\w-]+[_\w\d-]*)\]/','\1[@\2]',$sl);// foo[id]
+			$sl=preg_replace('/\[([_\w-]+[_\w\d-]*)\]/','*[@\1]',$sl); // [id]
+			$sl=preg_replace('/\[([_\w-]+[_\w\d-]*)=[\'"]?(.*?)[\'"]?\]/','[@\1="\2"]',$sl);// foo[id=foo]
+			$sl=preg_replace('/^\[/','*[',$sl);// [id=foo]
+			$sl=preg_replace('/([_\w-]+[_\w\d-]*)\#([_\w-]+[_\w\d-]*)/','\1[@id="\2"]',$sl);//div#foo
+			$sl=preg_replace('/\#([_\w-]+[_\w\d-]*)/','*[@id="\1"]',$sl);// #foo
+			$sl=preg_replace('/([_\w-]+[_\w\d-]*)\.([_\w-]+[_\w\d-]*)/','\1[contains(concat("",@class," ")," \2 ")]',$sl);// div.foo
+			$sl=preg_replace('/\.([_\w-]+[_\w\d-]*)/','*[contains(concat("",@class," ")," \1 ")]',$sl);// .foo
+			$sl=preg_replace('/([_\w-]+[_\w\d-]*):first-child/','*/\1[position()=1]',$sl);//div:first-child
+			$sl=preg_replace('/([_\w-]+[_\w\d-]*):last-child/','*/\1[position()=last()]',$sl);//div:last-child
+			$sl=str_replace(':first-child','*/*[position()=1]',$sl);// :first-child
+			$sl=str_replace(':last-child','*/*[position()=last()]',$sl);// :last-child
+			$sl=preg_replace('/:nth-last-child\((\d+)\)/','[position()=(last()-(\1 - 1))]',$sl);// :nth-last-child
+			$sl=preg_replace('/([_\w-]+[_\w\d-]*):nth-child\((\d+)\)/','*/*[position()=\2and self::\1]',$sl);// div:nth-child
+			$sl=preg_replace('/:nth-child\((\d+)\)/','*/*[position()=\1]',$sl);//:nth-child
+			$sl=preg_replace('/([_\w-]+[_\w\d-]*):contains\((.*?)\)/','\1[contains(string(.),"\2")]',$sl);//:contains(Foo)
+			$sl=preg_replace('/>/','/',$sl);// >
+			$sl=preg_replace('/~/','/following-sibling::',$sl);// ~
+			$sl=preg_replace('/\+([_\w-]+[_\w\d-]*)/','/following-sibling::\1[position()=1]',$sl);//+
+			$sl=str_replace(']*',']',$sl);
+			$sl=str_replace(']/*',']',$sl);
+		}
+		$sl=implode('/descendant::',$sls);
+		$sl='descendant-or-self::' . $sl;
+		$sl=preg_replace('/(((\|)?descendant-or-self::):scope)/','.\3',$sl);// :scope
+		$sub_sls=explode(',',$sl);// $element
+		foreach($sub_sls as $key => $sub_sl){
+			$parts=explode('$',$sub_sl);
+			$sub_sl=array_shift($parts);
+			if(count($parts) && preg_match_all('/((?:[^\/]*\/?\/?)|$)/', $parts[0], $matches)){
+				$results=$matches[0];
+				$results[]=str_repeat('/..', count($results) - 2);
+				$sub_sl .= implode('',$results);
+			}
+			$sub_sls[$key]=$sub_sl;
+		}
+		$sl=implode(',',$sub_sls);
+		return $sl;
+	}
+}
+function matches($pattern, $data){
+	preg_match_all($pattern, $data, $matches);
+	return $matches;
+}
+function getdom($html,$selector){
+	if(!empty($html)){
+		$dh = new DOMDocument();
+		libxml_use_internal_errors(TRUE); //disable libxml errors
+		$domh->loadHTML($html);
+		libxml_clear_errors(); //remove errors for yucky html
+		$xp = new DOMXPath($domh);
+		$pokemon_row = $pokemon_xpath->query('//h2[@id]');
+		if($pokemon_row->length > 0){
+			foreach($pokemon_row as $row){
+				echo $row->nodeValue . "<br/>";
+			}
+		}
+	}
+}
+function pre($v,$x=0){echo('<pre>');print_r($v);echo('</pre>');if($x)exit;}
+$url = 'https://sabaiptv.blogspot.com/2019/12/fresh-iptv-m3u-playlist-30-december-2019.html';
+/*Examples*/
+//Dom::data($url)-> {to get any attribute call attribute name as function}
+$arr = Dom::data($url)->class(); //return all class name in page
+$arr = Dom::data($url)->id(); //return all id name in page
+$arr = Dom::data($url)->data_custom();  //return value of all custom data attribute in page
+$arr = Dom::data($url)->urls(); //return all urls in page
+$arr = Dom::data($url)->href(); //return all href in page
+$arr = Dom::data($url)->src(); //return all src in page
+$arr = Dom::data($url)->find('img')->attr('src'); //return all src in page
+$arr = Dom::data($url)->find('a')->attr('href'); //return all src in page
+$arr = Dom::data($url)->find('meta[name=viewport]')->attr('content'); //return all urls in page
+//$arr = Dom::data($url)->content();
+$arr = Dom::data($url)->find('.post-author');
+$arr = Dom::data($url)->urls(); //return all urls in page
+pre($arr);
