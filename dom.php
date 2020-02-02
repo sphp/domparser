ee0ede453bbec567ff131a90f12922c5703193f3
Shamim <35076893+sphp@users.noreply.github.com>
2020-02-02 19:28:22 +0600

    Update dom.php

--- dom.php
+++ dom.php
@@ -1,83 +1,102 @@
 <?php
+
 class dom{
 	public $elms, $doc, $content, $that;
-	static $data, $attrs   = ['class','id','href','src','data'];
-	static $domVars = [
-		'tag'	=>'tagName',
-		'val'	=>'nodeValue',
-		'type'	=>'nodeType',
-		'parent'=>'parentNode',
-		'child'	=>'childNodes',
-		'first'	=>'firstChild',
-		'last'	=>'lastChild',
-		'attr'	=>'attributes',
-		'text'	=>'textContent',
-		'prev'  =>'previousSibling'
+	static $method, $args, $attrs = ['class','id','href','src','alt','title','style','name','data'];
+	static $vars = [
+		'tag'	 =>'tagName',
+		'val'	 =>'nodeValue',
+		'type'	 =>'nodeType',
+		'parent' =>'parentNode',
+		'child'	 =>'childNodes',
+		'first'	 =>'firstChild',
+		'last' 	 =>'lastChild',
+		'attr'	 =>'attributes',
+		'prev'   =>'previousSibling',
+		'save'   =>'saveHTMLFile'
 	];
 	function __construct(){
-		if($this->doc===null){
+		$args = isone(self::$args);
+		if($this->doc === null){
 			$this->doc = new DOMDocument(); libxml_use_internal_errors(1); //disable libxml errors
+			$this->that = $this->elms = $this->doc; //copy of start;
 		}
-		$data = self::$data;
-		if(!empty($data)){
-			if(is_string($data)){
-				$this->content = $data;
-				$this->doc->loadHTML($data);
+		if(is_object($args)){
+			$this->content = $args->saveHTML();
+		}else{
+			switch (self::$method){
+				case 'url'  : if(filter_var($args, FILTER_VALIDATE_URL)) $this->content = self::getContent($args); break;
+				case 'data' : if(isHtml($args)){ $this->content=$args; } break;
+				default 	: if(method_exists($this->doc, $name)){return call_user_func_array(array($this->doc, $name), $args);}
 			}
-			else if(is_object($data)) $this->content = self::$data->saveHTML();
 		}
-		$this->that = $this->elms = $this->doc; //copy of start;
+		if($this->content!==null) $this->loadHTML($this->content);
 	}
 	static function __callStatic($name, $args){
-		if(filter_var($args[0], FILTER_VALIDATE_URL)) self::$data=self::getContent($args[0]);
-		else self::$$name = $args; return new self;
+		self::$method = $name;
+		self::$args = $args;
+		return new self;
 	}
 	function __get($name){
-		if(array_key_exists($name, self::$domVars)) $name = self::$domVars[$name];
+		if(array_key_exists($name, self::$vars)) $name = self::$vars[$name];
 		return property_exists($this->elms, $name) ? $this->elms->$name : null;
 	}
 	function __call($name, $args){
 		if(method_exists($this->doc, $name)){
 			return call_user_func_array(array($this->doc, $name), $args);
 		}
-		else if(in_array($name, self::$attrs)){
-			if(isset($args[0]) && is_string($args[0])){
-				if($name=='id') $this->elms = $this->doc->getElementById($args[0]);
-				else if($name=='class') $this->elms = $this->find('.'. $args[0]); //pre($this->elms,1);
-			}else{
-				$data = str_replace("'", '"', $this->content);
-				return self::inStrs($data, str_replace("_", '-', $name)."=\"", "\"");
-			}
-		}
 		else{
+			$args = isone($args);
 			$e = $this->getElementsByTagName($name);
-			if(!empty($e->length)){
-				$this->elms = isset($args[0]) && is_integer($args[0]) ? $e[$args[0]] : $e;
-			}
-			else{
-				$this->elms = $this->$name;
+			/** get tag elements. integer args for the specific element**/
+			if( isDomList($e) && $e->length>0 ) $this->elms = isset($args) && is_integer($args) ? $e[$args] : $e;
+			/** direct return dom object properties **/
+			else if( in_array($name, self::$vars) ) $this->elms = $this->$name;
+			else if( in_array($name, self::$attrs))
+				return self::inStrs(str_replace("'", '"', $this->content), str_replace("_", '-', $name)."=\"", "\"");
+			/** use id or class name in args to get element **/
+			else if( is_string($args) ){
+				if($name=='id') $this->elms = $this->getElementById($args);
+				else if($name=='class') $this->find('.'. $args);
+				else if($name=='attr') return self::inStrs(str_replace("'", '"', $this->content), str_replace("_", '-', $name)."=\"", "\"");
 			}
 		}
 		return $this;
 	}
-	function loadHTML($html){
-		$this->doc->loadHTML($html);
-	}
-	function loadURL($url){
-		$this->content = $this->getContent($url);
-		$this->doc->loadHTML($this->content);
-	}
 	function nodeHtml(DOMNode $elm){
-		$str=''; 
-		if($elm) foreach($elm->childNodes as $kid) $str .= trim($elm->ownerDocument->saveHTML($kid));
-		return $str;
+		$str='';if($elm) foreach($elm->childNodes as $kid) $str .= trim($elm->ownerDocument->saveHTML($kid)); return $str;
 	}
 	function html(){
-		if(!isset($this->elms->length)) return $this->nodeHtml( $this->elms );
-		$arr=[]; foreach($this->elms as $node) $arr[] = $this->nodeHtml($node); return implode(PHP_EOL, $arr);
+		$html = [];
+		if(isDomDoc($this->elms)) $html[] = $this->content;
+		if(isDomElm($this->elms)) $html[] = $this->nodeHtml( $this->elms );
+		else if(isDomList($this->elms)){
+			foreach($this->elms as $elm) $html[] = $this->nodeHtml($elm); 
+		}
+		return isone($html);
 	}
-	function save($path){
-		return $this->doc->saveHTMLFile($path);
+	function contains($str){
+		$elms = [];
+		if(isDomList($this->elms)){
+			foreach($this->elms as $elm)
+				if($elm->nodeValue !== str_ireplace($str,'',$elm->nodeValue)) $elms[]=$elm; //OR if(mb_stripos($elm->nodeValue, $str)!==false)
+			if(count($elms)>1) array_shift($elms);
+			$this->elms = isone($elms);
+		}
+		return $this;
+	}
+	function get(){
+		$args = arr(isone(func_get_args()));
+		$items=[]; foreach ($args as $arg) $items[$arg] = arrFilter($this->$arg());
+		return arrayCombiner($items);
+	}
+	function text(){
+		$text = [];
+		if(isset($this->elms->textContent)) return $this->elms->textContent;
+		else if(isDomList($this->elms)){
+			foreach($this->elms as $node) $text[] = trim($node->textContent);
+		}
+		return isone($text);
 	}
 	static function getContent($url, $exp=86400/*24hr*/, $dir='./temp/'){
 		extract(parse_url($url));
@@ -86,7 +105,7 @@ class dom{
 		$path .= self::normalizeStr( !empty(basename($url)) ? basename($url) : $host);//create data file name from target url
 		$path = strpos($path, '.html') ? $path : $path.'.html';
 		if(is_readable($path) && time()-filemtime($path) < $exp){
-			self::cacheClean($exp);
+			//self::cacheClean($exp);
 			return file_get_contents($path);
 		}else{
 			$curl = self::curlGet($url);
@@ -153,7 +172,6 @@ class dom{
 		for($i=1;$i<count($p);$i++) $m[]=explode($e,$p[$i])[0];
 		return $m;
 	}
-	
 	function between($start, $end){
 		$str = explode($start, $this->content,2)[1];
 		$this->content = explode($end, $str,2)[0];
@@ -170,9 +188,6 @@ class dom{
 		$this->doc->loadHTML($str);
 		return $this;
 	}
-	function nodes(){
-		return $this->elms;
-	}
 	function node($indx=0){
 		return $this->elms->item($indx);
 	}
@@ -181,32 +196,15 @@ class dom{
 		foreach ($this->elms as $node) $arr[] = $node->getAttribute($val);
 		return !empty($arr) ? $arr : null;
 	}
-	function innerText(){
-		$arr = [];
-		foreach ($this->elms as $node) $arr[] = trim($node->textContent);
-		return !empty($arr) ? $arr : null;
-	}
 	function find($sel){
 		if(!ctype_alpha($sel)){
 			$path = $this->toXPath($sel);
 			$xpath = new DOMXpath($this->doc);
 			$this->elms = $xpath->query($path);
+			//pre($this->elms);
 		}
 		else $this->$sel();
 		return $this;
-	}
-	function GetById($id){
-	    $this->elms = $this->doc->getElementById($id);
-	    return $this;
-	}
-	function GetByTagName($tag){
-	    $this->elms = $this->doc->getElementsByTagName($tag);
-	    return $this;
-	}
-	function GetByClass($class){
-	    $xpath = new DOMXPath($this->doc);
-	    $this->elms = $xpath->query( "//*[contains(concat(' ', normalize-space(@class), ' '), ' $class ')]" );
-	    return $this;
 	}
 	function toXPath($sel) { // remove spaces around operators
 		$sel = preg_replace('/\s*>\s*/', '>', $sel);
@@ -272,29 +270,61 @@ class dom{
 	    $str = str_replace('%', '-', $str);
 	    return $str;
 	}
-	static function cacheClean($force=false, $dir='./temp/'){
+	static function cacheClean($exp=60*60*24, $force=false, $dir='./temp/'){
 		ignore_user_abort(true);
 		set_time_limit(0);
 		ob_start();
+		$dirs = [];
 		foreach(dirIterator('./temp/') as $path=>$obj){
-			if($force || is_file($path) && time()-filemtime($path) >= 60*60*24) is_dir($path) ? rmdir($path) : unlink($path);
+			if(is_dir($path)) $dirs[] = $path;
+			else if($force || is_file($path) && time()-filemtime($path) >= $exp) @unlink($path);
 		}
+		foreach($dirs as $dir) @rmdir($dir);
 		ob_flush();
 		flush(); 
 	}
 }
+function arrayCombiner($arr){
+	$result = [];
+	for ($i=0; $i <count(reset($arr)) ; $i++) { 
+		foreach($arr as $key => $v){
+			$result[$i][$key] = $arr[$key][$i];
+		}
+	}
+	return $result;
+} 
 function isHtml($str){return $str != strip_tags($str) ? true:false;}
-function absPath($path){
-	$path = str_replace(['/', '\\'], 'DS', $path);
-	$relDirs = array_filter(explode('DS', $path), function($p){return $p!=='.';});
-	$absDirs = explode(DIRECTORY_SEPARATOR, getcwd());
-	foreach($relDirs as $dir) $dir=='..' ? array_pop($absDirs) : array_push($absDirs, $dir);
-	return implode(DIRECTORY_SEPARATOR, $absDirs);
+function isDomElm($obj){return is_object($obj) && $obj instanceof DOMElement;}
+function isDomList($obj){return is_array($obj) || (is_object($obj) && $obj instanceof DOMNodeList);}
+function isDomDoc($obj){return is_object($obj) && $obj instanceof DOMDocument;}
+
+function strBetween($str, $bgn, $end){
+	$str = substr($str, mb_strpos($str, $bgn) + strlen($bgn));
+	if(strstr($str, $end, true)!=false) $str=strstr($str,$end,true);
+	return $str;
 }
-function dirIterator($path, $dots=RecursiveDirectoryIterator::SKIP_DOTS){
-	return new RecursiveIteratorIterator(new RecursiveDirectoryIterator(absPath($path), $dots), RecursiveIteratorIterator::SELF_FIRST);
+if(!function_exists('absPath')){
+	function absPath($path){
+		$path = str_replace(['/', '\\'], 'DS', $path);
+		$relDirs = array_filter(explode('DS', $path), function($p){return $p!=='.';});
+		$absDirs = explode(DIRECTORY_SEPARATOR, getcwd());
+		foreach($relDirs as $dir) $dir=='..' ? array_pop($absDirs) : array_push($absDirs, $dir);
+		return implode(DIRECTORY_SEPARATOR, $absDirs);
+	}
 }
-if(!function_exists('one')){function one($var){return is_array($var) && count($var)===1 ? $var[0] : $var;}}
-if(!function_exists('pre')){function pre($var, $xit=0){echo('<pre>'); print_r($var);echo('</pre>');if($xit) exit;}}
-if(!function_exists('matches')){function matches($rp, $str){return preg_match_all($rp, $str, $match) ? $match : false;}}
-if(!function_exists('toArray')){function toArray($var,$sep=','){return is_string($var) ? explode($sep, $var) : $var;}}
+if(!function_exists('dirIterator')){
+	function dirIterator($path, $dots=RecursiveDirectoryIterator::SKIP_DOTS){
+		return new RecursiveIteratorIterator(new RecursiveDirectoryIterator(absPath($path), $dots), RecursiveIteratorIterator::SELF_FIRST);
+	}
+}
+if(!function_exists('arrFilter')){function arrFilter($v){return isIndex($v)?array_values(array_filter(array_unique($v))):$v;}}
+if(!function_exists('varName')){function varName($val){foreach($GLOBALS as $k=>$v) if($v===$val)return $k;return false;}}
+if(!function_exists('isone')){function isone($v){return (is_array($v)&&isIndex($v)&&count($v)===1)?isone($v[0]):$v;}}
+if(!function_exists('uKey')){function uKey($key){$p=arr($key,'_');$i=(int)end($p); return $p[0].'_'.++$i;}}
+if(!function_exists('matches')){function matches($p,$s){return preg_match_all($p,$s,$match)?$match:false;}}
+if(!function_exists('isIndex')){function isIndex($v){return array_values($v)===$v;}}
+if(!function_exists('isAssoc')){function isAssoc($v){return !empty($v)?is_string(array_keys($v)[0]):$v;}}
+if(!function_exists('pre')){function pre($v,$x=0){echo '<pre>';print_r($v);echo'</pre>';if($x)exit;}}
+if(!function_exists('arr')){function arr($v, $d=','){return !is_array($v)?explode($d,$v):$v;}}
+if(!function_exists('str')){function str($v, $d=','){return  is_array($v)?implode($d,$v):$v;}}
+if(!function_exists('cut')){function cut(&$arr,$k){$v=$arr[$k];unset($arr[$k]);return $v;}}
